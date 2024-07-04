ed089c152d0f02a53a775046c47ee562c6cad6a1
Yuri <ygonzoo@gmail.com>
2024-07-04 11:26:24 +0200

    🦄 install tailwind 🦄

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useEffect, useState } from 'react'
-import markdownit from 'markdown-it'
+import { useEffect, useState } from "react";
+import markdownit from "markdown-it";
 
 function App() {
+  const [terms, setTerms] = useState("");
 
-  const [terms, setTerms] = useState('')
-  
   useEffect(() => {
     const md = markdownit();
-    fetch('./public.md').then( async (content) => {
-      const data = await content.text()
-      setTerms(md.render(data))
+    fetch("./public.md").then(async (content) => {
+      const data = await content.text();
+      setTerms(md.render(data));
     });
   }, []);
- 
+
   return (
     <>
+      <h1 className="red text-3xl font-bold underline">Terms of Service</h1>
+
       <div className="prose" dangerouslySetInnerHTML={{ __html: terms }} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
